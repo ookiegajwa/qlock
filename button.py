e7675fe45a2500e771e319eb1b6a1c5a0e6389d7
Edward46282 <eoedward7353@gmail.com>
2026-01-17 14:06:26 -0500

    Create button.py

---
+++ button.py
@@ -0,0 +1,18 @@
+import homesecurity as hs
+import rpi_gpio as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+BUTTON_PIN = 23
+my_sensor = hs.Sensor(zone=1)
+
+def buttonPressed(channel):
+    if GPIO.input(channel) == GPIO.LOW:
+        hs.trip(my_sensor)
+
+
+GPIO.setup(BUTTON_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.add_event_detect(BUTTON_PIN, GPIO.BOTH, callback=buttonPressed, bouncetime=300)
+
+while True: 
+    time.sleep(1)
