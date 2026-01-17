6cb88a3c3df8eaab1874a96a077948f977d5d491
ookiegajwa <ookiegajwa@yahoo.ca>
2026-01-17 12:49:46 -0500

    Add arming/disarming and tripping logic

--- homesecurity.py
+++ homesecurity.py
@@ -1,5 +1,5 @@
 # Store system state
-armed = False
+state = 0  # 0=disarmed, 1=armed, 2=alarm
 sensors = []
 access_controls = []
 outputs = []
@@ -8,29 +8,45 @@ outputs = []
 # Classes for input/output events
 class Sensor:
     def __init__(self, zone: int):
-        # Setup is overridden by inheritors to set up the callbacks
-        self.setup = None
         self.zone = zone
 
 
 class AccessControl:
     def __init__(self, zone: int):
-        self.setup = None
         self.zone = zone
 
 
 class SimpleOutput:
     def __init__(self):
-        self.setup = None
-        self.alarm = None
-        self.stop_alarm = None
+        pass
 
+    def on_arm(self):
+        pass
+
+    def on_disarm(self):
+        pass
+
+    def on_alarm(self):
+        pass
 
 def trip(sensor: Sensor):
     """Callback called when a sensor trips"""
-    pass
+    global state
+    if state == 1:
+        state = 2
+        for output in outputs:
+            output.on_alarm()
+
 
 
 def toggle_arm(source: AccessControl):
     """Callback for when an access control device is authorized"""
-    pass
+    global state
+    if state == 0:
+        state = 1
+        for output in outputs:
+            output.on_arm()
+    else:
+        state = 0
+        for output in outputs:
+            output.on_disarm()
