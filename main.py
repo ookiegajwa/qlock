b48f0a94af8cb8f9b72107682c7fa00a3c2a6796
YannHS <124695203+YannHS@users.noreply.github.com>
2026-01-17 18:45:37 -0500

    back door "lock" new pin

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import time
 import homesecurity, lcd, button, status_led, arm_toggle, buzzer, motion, lock
 
 led = status_led.StatusLED(13, 19, 26)
-back_door = lock.DoorLock(17)
+back_door = lock.DoorLock(12)
 
 lcd.lcd_init()
 button.setup()
