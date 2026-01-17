5229bbdfb71d59faf4237ea30fce614ea421eb28
ookiegajwa <ookiegajwa@yahoo.ca>
2026-01-17 15:23:38 -0500

    Add buzzer output

---
+++ buzzer.py
@@ -0,0 +1,26 @@
+import homesecurity
+import rpi_gpio as GPIO
+import time
+
+class Buzzer(homesecurity.SimpleOutput):
+    def __init__(self, pin):
+        self.pin = pin
+        GPIO.setup(pin, GPIO.OUT)
+
+    def on_arm(self):
+        GPIO.output(self.pin, GPIO.HIGH)
+        time.sleep(0.5)
+        GPIO.output(self.pin, GPIO.LOW)
+
+    def on_disarm(self):
+        GPIO.output(self.pin, GPIO.HIGH)
+        time.sleep(0.15)
+        GPIO.output(self.pin, GPIO.LOW)
+        time.sleep(0.15)
+        GPIO.output(self.pin, GPIO.HIGH)
+        time.sleep(0.15)
+        GPIO.output(self.pin, GPIO.LOW)
+        time.sleep(0.15)
+
+    def on_alarm(self):
+        GPIO.output(self.pin, GPIO.HIGH)
--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import time
-import homesecurity, lcd, button, status_led, arm_toggle
+import homesecurity, lcd, button, status_led, arm_toggle, buzzer
 
-homesecurity.outputs.append(lcd)
 lcd.lcd_init()
 button.setup()
 arm_toggle.setup()
+homesecurity.outputs.append(lcd)
 homesecurity.outputs.append(status_led)
+homesecurity.outputs.append(buzzer.Buzzer(4))
 
 while True:
     lcd.update_status()
