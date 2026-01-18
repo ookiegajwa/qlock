332507b61341b4564c1ff995057fc29f91054536
ari_ <110702490+ar1gami@users.noreply.github.com>
2026-01-17 20:52:04 -0500

    Add Sentry SDK initialization
    Integrate Sentry SDK for error tracking.

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import time
 import homesecurity, lcd, button, status_led, arm_toggle, buzzer, motion, lock
+import sentry_sdk
 
+sentry_sdk.init(
+    dsn="https://a156bfaf9f2c6cddb4694a339b574c66@o4510728194883584.ingest.de.sentry.io/4510728333557840",
+    send_default_pii=True,
+)
 led = status_led.StatusLED(5, 6, 26)
 back_door = lock.DoorLock(13)
 
